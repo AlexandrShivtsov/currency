52f4c729dc8d64698a48468e9e718073f2e77efa
AlexandrShivtsov <amshivtsov@gmail.com>
2021-07-11 01:09:53 +0300

    add models

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class ContactUs(models.Model):
     email_from = models.EmailField(max_length=50)
     subject = models.CharField(max_length=64)
-    message = models.CharField(max_length=200)
+    message = models.CharField(max_length=200 )
