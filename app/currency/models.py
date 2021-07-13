58d577186f92ebe527788cac91a2059803c94592
AlexandrShivtsov <amshivtsov@gmail.com>
2021-07-14 00:47:24 +0300

    add new files

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 class ContactUs(models.Model):
     email_from = models.EmailField(max_length=50)
     subject = models.CharField(max_length=64)
-    message = models.CharField(max_length=200 )
+    message = models.CharField(max_length=200)
